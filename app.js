231459d3091d368ffef347e4a4c45cc20e0c09e3
ApisaraOsw <apisara.osw@gmail.com>
2025-01-22 16:26:15 +0700

    add projects page

--- app.js
+++ app.js
@@ -3,17 +3,36 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const projectRouter = express.Router();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+projectRouter.route("/").get((req,res) => {
+    res.render("projects", {
+        projects: [
+            {projectTitle: 'Customer1', projectDescription: 'Our Customer1', projectPeriod: '1 Month'},
+            {projectTitle: 'Customer2', projectDescription: 'Our Customer2', projectPeriod: '2 Month'},
+            {projectTitle: 'Customer3', projectDescription: 'Our Customer3', projectPeriod: '3 Month'},
+            {projectTitle: 'Customer4', projectDescription: 'Our Customer4', projectPeriod: '4 Month'},
+        ], 
+    });
+});
+projectRouter.route("/1").get((req,res) => {
+    res.send("Hello!! I'm Project 1");
+});
+app.use("/projects", projectRouter);
+
 app.get("/", (req,res) => {
-    res.send('Hello Webapp');
+    res.render('index', {username: 'ako'});
 });
 
-app.listen(port ,() => {
-    debug("Listening on port: " + chalk.blue(port));
+app.listen(PORT ,() => {
+    debug("Listening on port: " + chalk.blue(PORT));
 });
